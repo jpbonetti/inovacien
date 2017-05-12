d5559305e066169d8c31c31d974f74021391b57b
Inovacien <empresa@inovacien.com.br>
2017-05-12 20:09:16 -0300

    teste envio

--- wp-content/themes/inovacien/assets/js/phpMailerSent.js
+++ wp-content/themes/inovacien/assets/js/phpMailerSent.js
@@ -1,6 +1,26 @@
-$(document).ready(function() {
-    $('#formContato').ajaxForm(function() {
-        swal('Sucesso!', 'Retornaremos seu contato o mais breve possível', 'success');
-        $('#formContato').clearForm();
+$(function() {
+    $('#contact-form').validator();
+
+    $('#contact-form').on('submit', function(e) {
+        if (!e.isDefaultPrevented()) {
+            var url = "envia.php";
+
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function(data) {
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
+
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    if (messageAlert && messageText) {
+                        $('#contact-form').find('.messages').html(alertBox);
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
     });
 })
